38be539df72610f668815ca926985b2cf45c1013
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-16 15:56:53 -0700

    101-qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(and that piece of art is useful" - Dora Korpar, 2015-10-19\n);
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
 
 	return (1);
 }
