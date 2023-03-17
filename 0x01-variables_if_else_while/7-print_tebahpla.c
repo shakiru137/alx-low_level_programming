22a9231ab1505dc3f77f0f57ea79b8ec5e1568c6
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-17 16:04:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'z' ; d >= 'a' ; d--)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
