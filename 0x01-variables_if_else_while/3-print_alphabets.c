50a46121bb27c2bde9de80098d2ab433f46efdf9
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-17 12:43:10 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lower_case;
+	int upper_case;
+
+	for (lower_case = 'a' ; lower_case <= 'z' ; lower_case++)
+	{
+	putchar(lower_case);
+	}
+	for (upper_case = 'A' ; upper_case <= 'Z' ; upper_case++)
+	{
+	putchar(upper_case);
+	}
+
+	return (0);
+}
