67bd8bb3face7efa79200f327849cc7cdec61169
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-17 17:16:32 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,15 @@
 int main(void)
 {
 	int n;
+
 	for (n = '0' ; n <= '9' ; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
