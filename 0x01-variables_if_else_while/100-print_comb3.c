de8572d6e69d0bc27b8d932688caa3ae0746e54b
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-17 23:36:59 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0' ; x < '9' ; x++)
+	{
+		for (y = x + 1 ; y <= '9' ; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x == '8' && y == '9')
+			{	
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
