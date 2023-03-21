ba4a743821f566da4c0414427e8244be4c25b871
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-21 13:39:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet in lowercase into ten lines
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int alphabetx10;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (alphabetx10 = 'a' ; alphabetx10 <= 'z' ; alphabetx10++)
+		{
+			_putchar(alphabetx10);
+		}
+		_putchar('\n');
+	}
+	
+}
+
