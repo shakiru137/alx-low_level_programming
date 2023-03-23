8026de97e4123f397fee16b76e0c82f5529aee19
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-23 13:37:54 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+nclude <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +7,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
