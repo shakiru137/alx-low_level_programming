30afec42dc3d141b4f55104ad24e77e9104e425b
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-23 17:27:05 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,13 +12,13 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
 	for (i = 0 ; i <= n ; i++)
 	{
-		_putchar(95);
+	_putchar(95);
 	}
 	_putchar('\n');
 	}
