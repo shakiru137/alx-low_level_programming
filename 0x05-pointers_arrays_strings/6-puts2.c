c226b31bc8e967c3c2207512f6fd7993c23ce01b
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-28 04:51:22 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - function that print other character starting with first one
+ * @str: variable
+ * return: 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
