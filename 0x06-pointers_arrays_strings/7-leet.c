bd3c25f8f3452aaf9d63f4644bc6275d0bbe1c33
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-03-30 02:35:02 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,21 @@
 char *leet(char *s)
 {
 	int i = 0;
+	int j;
+	char a1[] = "aAeEoOtTlL";
+	char a2[] = "4433007711";
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if(s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if(s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if(s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if(s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a1[j])
+			{
+				s[i] = a2[j];
+			}
+		}
 		i++;
 	}
+
 	return (s);
 }
