0df3c1363841b5e18dfe116f11c1176209b76a61
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-07 01:36:30 -0700

    1-argd.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
