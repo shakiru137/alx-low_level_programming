e3b6e7c61495a77bb2606ff23b372d2990a91f69
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-11 11:18:48 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -23,7 +23,7 @@ char *_strdup(char *str)
 	{
 		return (0);
 	}
-	new_str = malloc(_strlen(str) * sizeof(*str));
+	new_str = malloc(_strlen(str) * sizeof(*str) + 1);
 	if (new_str == NULL)
 	{
 		return (NULL);
