62e3664e8dcc45e9f46ec38503787e5ee097ca3d
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-11 17:42:52 -0700

    3-alloc_concat.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * **alloc_grid - Write a function that returns a pointer to 
+ * alloc_grid - Write a function that returns a pointer to
  * a 2 dimensional array of integers.
  * @width: 2-D width
  * @height: 2-D height
