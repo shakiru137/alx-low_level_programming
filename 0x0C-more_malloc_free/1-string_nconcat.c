4f643866b2d3bfe6087e9334d581d56d9d7d3696
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-13 10:10:11 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	new_str = malloc(sizeof(char) * (len1 + n + 1));
 	if (new_str == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
