742777df6ab6cbf589e2c156b3e65f0f5847f114
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-13 05:13:32 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,6 +9,10 @@ void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
 
-	ptr = malloc(sizeof(int) * b);
+	ptr = malloc(sizeof(unsigned int) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
 	return (ptr);
 }
