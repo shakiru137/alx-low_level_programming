9e1bed5927321274336a832f6f2f03ff83706e76
Yusuf Shakiru Oluwasegun <yusufshakiruoluwasegun@gmail.com>
2023-04-13 20:05:06 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,24 @@
+#include "main.h"
 #include <stdlib.h>
+#include <string.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size) {
+    /* check for zero arguments */
+    if (nmemb == 0 || size == 0) {
+        return NULL;
+    }
+
+    /* allocate memory using malloc */
+    void *ptr = malloc(nmemb * size);
+
+    /* check if allocation failed */
+    if (ptr == NULL) {
+        return NULL;
+    }
+
+    /* zero out the memory */
+    _memset(ptr, 0, nmemb * size);
+
+    /* return a pointer to the allocated memory */
+    return ptr;
+}
