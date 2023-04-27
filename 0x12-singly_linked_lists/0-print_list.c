589f3a7609f3b65e8bce5dd1f71f8dcecaa45641
Yusuf Shakiru Oluwasegun <yusufshakiruoluwasegun@gmail.com>
2023-04-27 22:47:21 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,9 +5,18 @@
  *
  * Return: Number of element in each list accessed.
  */
+
+
+
+
+
+
+
+
+
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -15,9 +24,23 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		count++;
+		nodes++;
 		h = h->next;
 	}
-	return (count);
+	return (nodes);
 }
+
+
+
+
+	
+
+
+
+
+
+
+	
+	
+
+
