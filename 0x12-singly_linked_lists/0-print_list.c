08a89e8f81d3177e020c11f11c6a393712140780
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-27 08:03:24 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,8 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h)
 	{
 		if (h->str != NULL)
@@ -20,8 +22,8 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
-		count++;
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
