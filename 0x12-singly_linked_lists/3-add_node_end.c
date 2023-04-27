0fa8c62aecd67ff8d4b2b1c52e225bd3cf39910a
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-27 11:05:18 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,35 +1,45 @@
 #include "lists.h"
 /**
- * add_node_end - function that adds a new node at beginning of a list_t list.
- * @head: pointer pointing to pointer pointing to struct list.
- * @str: pointer pointint to list string.
+ * add_node_end - function that adds a new node at the end of a list_t list.
+ * @head: pointer to the first node of list.
+ * @str: pointer to string.
  *
- * Return: head.
+ * Return: NULL if fail, starting the add of the list.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *add_node_end;
-	int length = 0;
+	list_t *add_node_end, *tmp;
 
-	add_node_end = malloc(sizeof(list_t));
-	if (add_node_end == NULL)
+	if (!head)
 		return (NULL);
 
-	if (add_node_end == NULL)
+	add_node_end = malloc(sizeof(list_t));
+	if (!add_node_end)
 		return (NULL);
 
 	add_node_end->str = strdup(str);
-	if (add_node_end->str == NULL)
+	if (!add_node_end->str)
 	{
 		free(add_node_end);
 		return (NULL);
 	}
-	while (str[length])
-		length++;
 
-	add_node_end->next = *head;
-	add_node_end->len = length;
-	*head = add_node_end;
+	add_node_end->len = strlen(str);
+	add_node_end->next = NULL;
+
+	if (!*head)
+	{
+		*head = add_node_end;
+		return (add_node_end);
+	}
+
+	tmp = *head;
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = add_node_end;
 	return (add_node_end);
 }
-
