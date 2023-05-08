8835685033d38390b174efcb88cba7c0277aaf15
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-05-08 09:07:40 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: Pointer to file name.
+ * @text_content: Pointer to file content.
+ *
+ * Return: 1 on SUCCESS, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fdescriptor;
+	int toWrite, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	/* O_CREAT indicate the file should be created if not exist
+	 * O_RDWR indicate that the file should be open for both read and write mode
+	 * O_TRUNC indicate if the file already exit for writing, it content should be deleted or truncate back to 0
+	 * 600 indicate permmission rw- --- --- for the created file */
+	fdescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	if (fdescriptor < 0)
+		return (-1);
+	/* write to the text_content if the file is not empty */
+	if (text_content)
+	{
+		/* loop through and count number of characters present */
+		while (text_content[i])
+			i++;
+		/* finally we write all the text_content to the file using */
+		toWrite = write(fdescriptor, text_content, i);
+		if (toWrite != i)
+			return (-1);
+	}
+	/* close the file opened */
+	close(fdescriptor);
+
+	return (1);
+}
