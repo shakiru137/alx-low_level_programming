570068f6defef74d1005f3f246500ca4fdb469f5
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-05-08 13:06:01 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #define BUFFER_SIZE 1024
+
 int _putchar(char ch);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
