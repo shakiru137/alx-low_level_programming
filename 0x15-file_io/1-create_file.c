18aebfe817522b381665f3d331f6cd661d84fc55
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-05-08 09:28:18 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	 * be deleted or truncate back to 0
 	 * 600 indicate permmission rw- --- --- for the created file
 	 */
-	fdescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fdescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fdescriptor < 0)
 		return (-1);
 	/* write to the text_content if the file is not empty */
