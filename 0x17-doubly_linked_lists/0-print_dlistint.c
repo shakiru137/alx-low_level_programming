d92f2d74b557fef50293a3c1c2b1c1be3ca97c9e
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-01-25 09:18:58 -0800

    updated

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t num = 0;
-	const dlistint_t *temp = h;
+	int num = 0;
 
-	while (temp != NULL)
+	while (h)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		num++;
 	}
 	return (num);
