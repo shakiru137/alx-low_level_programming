d14c3d2e5b550c56561c144af3e4fbb8dd14e5b7
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-02-16 19:53:06 +0100

    updated

--- 0x1A-hash_tables/0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - function that adds an element to the hash table
- * @ht: the hash table you want to add or update the key/value to
- * @key: is the key. key can not be an empty string
- * @value: the value associated with the key.
- * value must be duplicated. value can be an empty string
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
