768d4f4423fa49428fa87160c927fa1b4b511def
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-02-16 18:55:53 +0100

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,21 +11,36 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (ht == NULL; || key == NULL || *key == '\0')
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0); /* Invalid input */
 
-	index = key
+	index = hash_djb2((unsigned char *)key) % ht->size;
 
-	if ((ht->key != NULL) && (ht->value != NULL))
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		head = ht->next;
-		ht->key = key;
-		ht->value = value;
+		free(new_node);
+		return (0);
 	}
-	else
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
 	{
-		ht->key = key;
-		ht->value = value;
+		free(new_node);
+		free(new_node->key);
+		return (0);
 	}
-	return (ht);
+
+	/* Insert new node into the hash table */
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
