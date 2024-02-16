ab2514cc32426621ff7e530aa651966c385bad4e
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-02-16 03:38:34 -0800

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,15 +10,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_table_t));
-	if (ht->array == NULL)
-		return (NULL);
-	return (ht);
 
-return (ht);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_table_t) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
